752b17b5ca61aa9dc821b646347415eb5f7df7be
stanley <stanleyw2014@163.com>
2019-04-12 22:54:34 +0800

    add controller

---
+++ src/main/java/com/obs/eggplant/EggplantApplication.java
@@ -0,0 +1,13 @@
+package com.obs.eggplant;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EggplantApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EggplantApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/obs/eggplant/EggplantApplicationTests.java
@@ -0,0 +1,16 @@
+package com.obs.eggplant;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EggplantApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
